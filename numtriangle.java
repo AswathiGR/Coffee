22c4dcaa5b4c6e60d6bece16216d361770c621fb
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-09-05 16:05:32 +0530

    Create numtriangle.java

---
+++ numtriangle.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+
+public class numtriangle
+{
+
+  public static void main(String args[])
+
+			{
+   	          int i=1,j,n=10;
+               for(i=1;i<=n;i++)
+               {  
+                   if (i>=2)
+                   {
+                        int m=i;
+	                    for(j=1;j<=i;j++)
+                       {
+                          m++;
+             
+                          System.out.print(i);
+                        
+                     
+                         
+                    
+                       }
+                         System.out.println("");
+                      
+                   }
+                   else
+                   {
+                           System.out.println(i); 
+                           
+                   }
+                   
+               }
+                      
+              
+			}     
+               
+             
+          
+}
+            
